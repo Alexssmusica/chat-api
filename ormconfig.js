177fdf3ae22ef3ff23d08afb2658e33ca77d2340
Alex Santos de Souza <alexssmusica@gmail.com>
2021-04-20 16:11:55 -0300

    Add typeOrm

---
+++ ormconfig.js
@@ -0,0 +1,12 @@
+const configSqlite = {
+    type: "sqlite",
+    database: "./src/database/database.sqlite",
+    migrations: ["./src/database/migrations/**.ts"],
+    entities: ["./src/models/**.ts"],
+    logging: false,
+    cli: {
+        migrationsDir: "./src/database/migrations",
+    },
+};
+
+module.exports = configSqlite;
---
+++ src/entities/Setting.ts
@@ -0,0 +1,12 @@
+import { Entity } from 'typeorm';
+
+@Entity()
+class Setting {
+    id: string;
+    username: string;
+    chat: boolean;
+    created_at: Date;
+    updated_at: Date;
+}
+
+export { Setting };
